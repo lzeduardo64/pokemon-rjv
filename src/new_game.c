f40898d3dc77c4e5d76952790520576705e7d8af
Sangetsuki <lzeduardo64@gmail.com>
2021-03-05 11:58:33 -0300

    trocado ä pelo ã

--- src/new_game.c
+++ src/new_game.c
@@ -208,7 +208,7 @@ void NewGameInitData(void)
     ResetContestLinkResults();
     gSaveBlock2Ptr->expShare = 0;
     memset(&gSaveBlock2Ptr->itemFlags, 0, sizeof(gSaveBlock2Ptr->itemFlags));
-    gSaveBlock1Ptr->coolNumber = 0x20;
+    gSaveBlock1Ptr->coolNumber = Random() % 0xFF;
 }
 
 static void ResetMiniGamesResults(void)
