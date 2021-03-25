e8511dc255fc65004125aa578e8462fd6847d81a
Sangetsuki <lzeduardo64@gmail.com>
2021-03-25 02:05:36 -0300

    Revert "send builded rom to discord"
    This reverts commit 79e8857abf039fd865eae28648c6165c5ab3809f.

--- .github/calcrom/webhook.sh
+++ .github/calcrom/webhook.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -ex
 
 build_name=$1
-extension=_modern.gba
-map_file=$build_name$extension
+map_file=$build_name.map
 if [ ! -f $map_file ]; then
   echo "$map_file does not exist!"
   exit 1
 fi
 
 output=$(perl $(dirname "$0")/calcrom.pl $build_name.map | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
-zip -r pokeemerald_modern.zip pokeemerald_modern.gba
-bash $(dirname "$0")/discord.sh --webhook-url=$CALCROM_DISCORD_WEBHOOK_URL --file pokeemerald_modern.zip --username "yooo" --text "teste123"
+curl -d "{\"username\": \"$CALCROM_DISCORD_WEBHOOK_USERNAME\", \"avatar_url\": \"$CALCROM_DISCORD_WEBHOOK_AVATAR_URL\", \"content\":\"\`\`\`$build_name progress:\\n$output\`\`\`\"}" -H "Content-Type: application/json" -X POST "$CALCROM_DISCORD_WEBHOOK_URL"
