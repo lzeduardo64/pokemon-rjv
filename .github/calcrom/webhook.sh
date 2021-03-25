a5abebc3f38688ae7d4fe98b000949428b37e632
Sangetsuki <lzeduardo64@gmail.com>
2021-03-25 02:04:32 -0300

    Revert "remeber kids, never forget ""
    This reverts commit 453c790f3d94b54c33bb6a9626bce82ca6d2bd2b.

--- .github/calcrom/webhook.sh
+++ .github/calcrom/webhook.sh
@@ -10,4 +10,4 @@ fi
 
 output=$(perl $(dirname "$0")/calcrom.pl $build_name.map | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
 zip -r pokeemerald_modern.zip pokeemerald_modern.gba
-bash $(dirname "$0")/discord.sh --webhook-url="$CALCROM_DISCORD_WEBHOOK_URL" --file pokeemerald_modern.zip --username "yooo" --text "teste123"
+bash $(dirname "$0")/discord.sh --webhook-url=$CALCROM_DISCORD_WEBHOOK_URL --file pokeemerald_modern.zip --username "yooo" --text "teste123"
