3e2b9a70c4881481f9d5e16e2a11bfcadd113a71
Sangetsuki <lzeduardo64@gmail.com>
2021-04-17 11:30:27 -0300

    limit pokedex to gen 6

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -118,7 +118,7 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TURTWIG,
+    SPECIES_ROWLET,
     SPECIES_FENNEKIN,
     SPECIES_OSHAWOTT,
 };
